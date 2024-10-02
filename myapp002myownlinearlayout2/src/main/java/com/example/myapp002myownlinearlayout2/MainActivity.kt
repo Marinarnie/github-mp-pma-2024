6a3faa89e0a467f33cc2efad57250acb16a82499
marinarnie <marina.pakhomova@tul.cz>
2024-10-02 22:51:31 +0200

    1. done

--- myapp002myownlinearlayout2/src/main/java/com/example/myapp002myownlinearlayout2/MainActivity.kt
+++ myapp002myownlinearlayout2/src/main/java/com/example/myapp002myownlinearlayout2/MainActivity.kt
@@ -11,39 +11,4 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-
-        val etbtn = findViewById<R.id.et>()
-        etbtn.setOnClickListener {
-            calculateArea()
-        }
-
-        etbtn2.setOnClickListener {
-            clearInputs()
-        }
-    }
-
-    private fun calculateArea() {
-        val widthInput = etsirka.text.toString()
-        val lengthInput = etdelka.text.toString()
-
-        if (widthInput.isNotEmpty() && lengthInput.isNotEmpty()) {
-            val width = widthInput.toDouble()
-            val length = lengthInput.toDouble()
-            val area = width * length
-
-            etText.text = "Plocha: $area m²"
-        } else {
-            etText.text = "Zadejte obě hodnoty."
-        }
-    }
-
-    private fun clearInputs() {
-        etsirka.text.clear()
-        etdelka.text.clear()
-        etText.text = "Plocha v m²"
-    }
-}
