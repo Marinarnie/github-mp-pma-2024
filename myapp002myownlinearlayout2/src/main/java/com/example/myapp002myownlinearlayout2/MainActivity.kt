5fae2134319d067c560048cf667ff6e4d2a9be1e
marinarnie <marina.pakhomova@tul.cz>
2024-10-02 23:35:30 +0200

    2. done

--- myapp002myownlinearlayout2/src/main/java/com/example/myapp002myownlinearlayout2/MainActivity.kt
+++ myapp002myownlinearlayout2/src/main/java/com/example/myapp002myownlinearlayout2/MainActivity.kt
@@ -9,6 +9,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+
 
 
