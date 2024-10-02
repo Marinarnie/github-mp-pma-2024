e7bac1fcf71ac4d480d2c8a6392b774dcd8fbc84
marinarnie <marina.pakhomova@tul.cz>
2024-10-02 08:21:48 +0200

    1. done

---
+++ myapp002myownlinearlayout2/src/main/java/com/example/myapp002myownlinearlayout2/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.myapp01linearlayout
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val etbtn = findViewById<R.id.et>()
+        etbtn.setOnClickListener {
+            calculateArea()
+        }
+
+        etbtn2.setOnClickListener {
+            clearInputs()
+        }
+    }
+
+    private fun calculateArea() {
+        val widthInput = etsirka.text.toString()
+        val lengthInput = etdelka.text.toString()
+
+        if (widthInput.isNotEmpty() && lengthInput.isNotEmpty()) {
+            val width = widthInput.toDouble()
+            val length = lengthInput.toDouble()
+            val area = width * length
+
+            etText.text = "Plocha: $area m²"
+        } else {
+            etText.text = "Zadejte obě hodnoty."
+        }
+    }
+
+    private fun clearInputs() {
+        etsirka.text.clear()
+        etdelka.text.clear()
+        etText.text = "Plocha v m²"
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "MyApp01LinearLayout"
 include(":app")
- 
+include(":myapp002myownlinearlayout2")
