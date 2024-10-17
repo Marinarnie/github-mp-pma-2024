963e2c8ae5d8a4157660181d52ec51acf597d3fc
marinarnie <marina.pakhomova@tul.cz>
2024-10-17 11:44:14 +0200

    myapp6

--- myapp007toastsnackbar/src/main/java/cz/tul/myapp007toastsnackbar/MainActivity.kt
+++ myapp007toastsnackbar/src/main/java/cz/tul/myapp007toastsnackbar/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 import cz.tul.myapp007toastsnackbar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -21,8 +22,17 @@ class MainActivity : AppCompatActivity() {
         binding.btnShowtoast.setOnClickListener {
 
             val toast = Toast.makeText( this, "Nazdar - mám hlad", Toast.LENGTH_LONG)
+            toast.show()
+        }
 
-            toast.show() }
+        binding.btnShowsnackbar.setOnClickListener {
+            Snackbar.make(binding.root, "Já jsem snackbar", Snackbar.LENGTH_LONG)
+                .setDuration(7000)
+                .setAction("Zavřít") {
+                    Toast.makeText(this, "Zavírám snackbar", Toast.LENGTH_LONG).show()
+                }
+                .show()
+        }
 
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
