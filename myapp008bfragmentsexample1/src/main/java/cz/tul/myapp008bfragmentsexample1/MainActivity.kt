ab357b8be03d585ce6da969bf5aa96f53f8ef7fd
marinarnie <marina.pakhomova@tul.cz>
2024-10-24 15:19:54 +0200

    myapp6

--- myapp008bfragmentsexample1/src/main/java/cz/tul/myapp008bfragmentsexample1/MainActivity.kt
+++ myapp008bfragmentsexample1/src/main/java/cz/tul/myapp008bfragmentsexample1/MainActivity.kt
@@ -3,14 +3,20 @@ package cz.tul.myapp008bfragmentsexample1
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import cz.tul.myapp008bfragmentsexample1.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+
     }
+
 }
