6dcec69a68e4c25928645e3f5919fe8fff279157
marinarnie <marina.pakhomova@tul.cz>
2024-11-13 15:22:34 +0100

    k

--- kalendar/src/main/java/com/example/kalendar/event.kt
+++ kalendar/src/main/java/com/example/kalendar/event.kt
@@ -2,6 +2,6 @@ package com.example.kalendar
 
 data class event (
     val title: String,
-    val date: String, // Můžete použít Date nebo LocalDate, pokud chcete
-    val category: String // Např. "osobní", "pracovní", atd.
+    val date: String,
+    val category: String
 )
