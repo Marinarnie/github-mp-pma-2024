9cc70f9d29792c83cf01db694de7b9dc20374ae7
marinarnie <marina.pakhomova@tul.cz>
2024-12-12 12:32:13 +0100

    start layout

--- myapp016avanocniappka/src/main/java/com/example/myapp016avanocniappka/MainActivity.kt
+++ myapp016avanocniappka/src/main/java/com/example/myapp016avanocniappka/MainActivity.kt
@@ -6,8 +6,6 @@ import android.os.Handler
 import android.widget.TextView
 import java.util.*
 import android.animation.ObjectAnimator
-import android.graphics.Color
-import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.core.animation.doOnEnd
