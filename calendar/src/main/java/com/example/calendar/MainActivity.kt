6f1068dfb7b4b2bb6f842194df5a794f60a3f855
marinarnie <marina.pakhomova@tul.cz>
2025-01-03 15:21:11 +0100

    calendar - základ

--- calendar/src/main/java/com/example/calendar/MainActivity.kt
+++ calendar/src/main/java/com/example/calendar/MainActivity.kt
@@ -4,35 +4,36 @@ import android.os.Bundle
 import android.widget.CalendarView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import java.util.*
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
-    lateinit var dateTV: TextView
+
+    lateinit var vybranyDatum: TextView
     lateinit var calendarView: CalendarView
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setLocale(Locale("cs", "CZ"))
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Nastavení češtiny jako výchozího jazyka
-        val locale = Locale("cs", "CZ")
+        vybranyDatum = findViewById(R.id.etVybranyDatum)
+        calendarView = findViewById(R.id.etCalendarView)
+
+        //listener pro změnu data v kalendáři
+        calendarView.setOnDateChangeListener(
+            //format data do češtiny
+                CalendarView.OnDateChangeListener { view, year, month, dayOfMonth ->
+                    val formattedDate = String.format(Locale("cs"), "%02d.%02d.%d", dayOfMonth, month + 1, year)
+                    vybranyDatum.text = formattedDate
+                })
+    }
+    private fun setLocale(locale: Locale) {
         Locale.setDefault(locale)
         val config = resources.configuration
         config.setLocale(locale)
+        // Aktualizace konfigurace aplikace
+        createConfigurationContext(config)
         resources.updateConfiguration(config, resources.displayMetrics)
-
-        // Inicializace TextView a CalendarView
-        dateTV = findViewById(R.id.idTVDate)
-        calendarView = findViewById(R.id.calendarView)
-
-        // Nastavení prvního dne týdne na pondělí
-        val calendar = Calendar.getInstance(locale)
-        calendar.firstDayOfWeek = Calendar.MONDAY
-
-        // Posluchač pro změnu datumu
-        calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
-            val date = "$dayOfMonth-${month + 1}-$year"
-            dateTV.text = date
-        }
     }
 }
