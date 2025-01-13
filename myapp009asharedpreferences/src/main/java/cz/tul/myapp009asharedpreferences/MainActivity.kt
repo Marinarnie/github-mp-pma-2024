00f29dbc39e8d15568dfefdc85eea17040e223be
marinarnie <marina.pakhomova@tul.cz>
2025-01-13 18:41:23 +0100

    1. calendar - základ 2. okenko pridání události 3. recylerview s seznamem přidaných událostí 4. tlacitko smazat 5. spinner xml 6. filtr 7. kategorie 8. uprava nazvu

--- myapp009asharedpreferences/src/main/java/cz/tul/myapp009asharedpreferences/MainActivity.kt
+++ myapp009asharedpreferences/src/main/java/cz/tul/myapp009asharedpreferences/MainActivity.kt
@@ -1,22 +1,69 @@
 package cz.tul.myapp009asharedpreferences
 
+import android.content.Context
 import android.os.Bundle
 import android.renderscript.ScriptGroup.Binding
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import cz.tul.myapp009asharedpreferences.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-    }
 
+
+        // Inicializace bindingu
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Přístup k SharedPreferences
+        val sharedPref = getSharedPreferences("myPref", Context.MODE_PRIVATE)
+        val editor = sharedPref.edit()
+
+        // Funkcionalita pro uložení dat
+        binding.btnSave.setOnClickListener {
+            val name = binding.etName.text.toString()
+            val ageString = binding.etAge.text.toString().trim()
+
+            if (ageString.isBlank()) {
+                Toast.makeText(this, "Prosím, vyplňte věk", Toast.LENGTH_SHORT).show()
+            } else {
+                val age = ageString.toInt()
+                val isAdult = binding.cbAdult.isChecked
+                if ((age < 18 && isAdult) || (age >= 18 && !isAdult)) {
+                    Toast.makeText(this, "Musíte být starší než 17 let", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Uloženo", Toast.LENGTH_SHORT).show()
+                    editor.apply {
+                        putString("name", name)
+                        putInt("age", age)
+                        putBoolean("isAdult", isAdult)
+                        apply()
+                    }
+                }
+            }
+        }
+
+
+        // Funkcionalita pro načtení dat
+        binding.btnLoad.setOnClickListener {
+            val name = sharedPref.getString("name", null)
+            val age = sharedPref.getInt("age", 0)
+            val isAdult = sharedPref.getBoolean("isAdult", false)
+
+            binding.etName.setText(name)
+            binding.etAge.setText(age.toString())
+            binding.cbAdult.isChecked = isAdult
+        }
+
+
+
+
+    }
 }
